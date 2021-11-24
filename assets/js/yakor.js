ead54d7c95570fd5234179f0d6980f1f458fcb5d
Dyachuk-P <74014985+Dyachuk-P@users.noreply.github.com>
2021-11-25 00:48:07 +0200

    Delete assets directory

--- assets/js/yakor.js
+++
@@ -1,6 +0,0 @@
-$(".nav").on("click", "a", function(event) {
-    event.preventDefault();
-    var anchorId  = $(this).attr("href");
-    scrollingDistance = $(anchorId).offset().top - $(".nav").height();
-    $("html, body").animate({scrollTop: scrollingDistance}, 800);
-});
