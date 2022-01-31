9a14b24045ab53c503b31a33e8889992f1a2284c
Dyachuk-P <74014985+Dyachuk-P@users.noreply.github.com>
2022-01-31 14:36:22 +0200

    Update timer.js

--- assets/js/timer.js
+++ assets/js/timer.js
@@ -1,48 +1,49 @@
 function getTimeRemaining(endtime) {
-    var t = Date.parse(endtime) - Date.parse(new Date());
-    var seconds = Math.floor((t / 1000) % 60);
-    var minutes = Math.floor((t / 1000 / 60) % 60);
-    // var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-    // var days = Math.floor(t / (1000 * 60 * 60 * 24));
-    return {
-      total: t,
-    //   days: days,
-    //   hours: hours,
-      minutes: minutes,
-      seconds: seconds
-    };
-  }
-  
-  function initializeClock(id, endtime) {
-    var clock = document.getElementById(id);
-    // var daysSpan = clock.querySelector(".days");
-    var hoursSpan = clock.querySelector(".hours");
-    var minutesSpan = clock.querySelector(".minutes");
-    var secondsSpan = clock.querySelector(".seconds");
-  
-    function updateClock() {
-      var t = getTimeRemaining(endtime);
-  
-      if (t.total <= 0) {
-        document.getElementById("countdown").className = "hidden";
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var seconds = Math.floor((t / 1000) % 60);
+  var minutes = Math.floor((t / 1000 / 60) % 60);
+  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+  var days = Math.floor(t / (1000 * 60 * 60 * 24));
+  return {
+    'total': t,
+    'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
+ 
+function initializeClock(id, endtime) {
+  var clock = document.getElementById(id);
+  var daysSpan = clock.querySelector('.days');
+  var hoursSpan = clock.querySelector('.hours');
+  var minutesSpan = clock.querySelector('.minutes');
+  var secondsSpan = clock.querySelector('.seconds');
+ 
+  function updateClock() {
+    var t = getTimeRemaining(endtime);
+ 
+    daysSpan.innerHTML = t.days;
+    hoursSpan.innerHTML = ('' + t.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+ 
+    if (t.total <= 0) {
+      document.getElementById("countdown").className = "hidden";
         document.getElementById("deadline-message").className = "visible";
+        // let end = document.getElementById('.timer-title')
         end.style.display = 'none';
+        timerEnd.style.display = 'none';
         clearInterval(timeinterval);
-        return true;
-      }
-  
-    //   daysSpan.innerHTML = t.days;
-    //   hoursSpan.innerHTML = ("0" + t.hours).slice(-2);
-      minutesSpan.innerHTML = ("0" + t.minutes).slice(-2);
-      secondsSpan.innerHTML = ("0" + t.seconds).slice(-2);
     }
-  
-    updateClock();
-    var timeinterval = setInterval(updateClock, 1000);
   }
-  
-  //var deadline = "January 01 2018 00:00:00 GMT+0300"; //for Ukraine
-  var deadline = new Date(Date.parse(new Date()) + 1975 * 1000); // for endless timer
+ 
+  updateClock();
+  var timeinterval = setInterval(updateClock, 1000);
+}
+var deadline="February 05 2022 00:00:00 GMT+0300";
+  // var deadline = new Date(Date.parse(new Date()) + 10 * 24 * 58 * 57 * 1050); // for endless timer
   const end = document.querySelector('.countdown')
-  
+  const timerEnd = document.querySelector('.timer-title')
+  localStorage.setItem('cart', JSON.stringify(deadline))
   initializeClock("countdown", deadline);
